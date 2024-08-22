8fb55b1486c598175cf0c8f6522e4f3a7de4c5de
elkanajnrmanullang <elkanamanullang7@gmail.com>
2024-08-22 18:51:19 +0700

    beranda sisa footer

--- js/main.js
+++ js/main.js
@@ -1,32 +1,42 @@
 //SWIPER SLIDER
 var swiper = new Swiper(".bg-slider-thumbs", {
-    loop: true,
-    spaceBetween: 0,
-    slidesPerView: 0,
-  });
-  var swiper2 = new Swiper(".bg-slider", {
-    loop: true,
-    spaceBetween: 0,
-    thumbs: {
-      swiper: swiper,
-    },
-  });
+  loop: true,
+  spaceBetween: 0,
+  slidesPerView: 0,
+});
+var swiper2 = new Swiper(".bg-slider", {
+  loop: true,
+  spaceBetween: 0,
+  thumbs: {
+    swiper: swiper,
+  },
+});
 
-  //Navbar Effect on Scroll
-  window.addEventListener("scroll", function(){
-    const header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
-  });
+//Navbar Effect on Scroll
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
 
-  //Navbar Menu
-  const menuBtn = document.querySelector(".nav-menu-btn");
-  const closeBtn = document.querySelector(".nav-close-btn");
-  const navigation = document.querySelector(".navigation");
+//Navbar Menu
+const menuBtn = document.querySelector(".nav-menu-btn");
+const closeBtn = document.querySelector(".nav-close-btn");
+const navigation = document.querySelector(".navigation");
 
-  menuBtn.addEventListener("click", () =>{
-    navigation.classList.add("active");
-  });
+menuBtn.addEventListener("click", () => {
+  navigation.classList.add("active");
+});
 
-  closeBtn.addEventListener("click", () =>{
-    navigation.classList.remove ("active");
-  });
+closeBtn.addEventListener("click", () => {
+  navigation.classList.remove("active");
+});
+
+//Image Slider
+var swiper = new Swiper(".destination", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
